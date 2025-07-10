4330072972a7602607007d3ae57cc38869420561
Grazielli-07 <grazischolante@gmail.com>
2025-07-10 16:47:34 -0300

    Update Vetor11.java

--- VeteMat/Vetor11.java
+++ VeteMat/Vetor11.java
@@ -1,4 +1,5 @@
-
+//Fac¸a um programa que preencha um vetor com 10 numeros reais, calcule e mostre a ´
+//quantidade de numeros negativos e a soma dos n ´ umeros positivos desse vetor.
 import java.util.Scanner;
 
 public class Vetor11 {
