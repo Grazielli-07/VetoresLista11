3b7fd0a4c64053a868daf4f145428eeee101556d
Grazielli-07 <grazischolante@gmail.com>
2025-07-10 16:49:59 -0300

    Update Vetor19.java

--- VeteMat/Vetor19.java
+++ VeteMat/Vetor19.java
@@ -1,3 +1,5 @@
+//Fac¸a um vetor de tamanho 50 preenchido com o seguinte valor: (i + 5 ∗ i)%(i + 1), sendo
+//i a posic¸ao do elemento no vetor. Em seguida imprima o vetor na tela.
 public class Vetor19 {
     public static void main(String[] args) {
         
