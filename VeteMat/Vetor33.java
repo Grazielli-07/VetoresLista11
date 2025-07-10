5f31d670ace48bfad51f59ca6435321675c6167f
Grazielli-07 <grazischolante@gmail.com>
2025-07-10 16:55:07 -0300

    Update Vetor33.java

--- VeteMat/Vetor33.java
+++ VeteMat/Vetor33.java
@@ -1,4 +1,6 @@
-
+//Fac¸a um programa que leia um vetor de 15 posic¸oes e o compacte, ou seja, elimine as ˜
+//posic¸oes com valor zero. Para isso, todos os elementos ˜ a frente do valor zero, devem ser `
+//movidos uma posic¸ao para tr ˜ as no vetor.
 import java.util.Scanner;
 
 public class Vetor33 {
