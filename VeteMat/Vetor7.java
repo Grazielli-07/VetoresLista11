1bd29e3fea7dec7764440362879ad0843f0eb013
Grazielli-07 <grazischolante@gmail.com>
2025-07-10 16:46:21 -0300

    Update Vetor7.java

--- VeteMat/Vetor7.java
+++ VeteMat/Vetor7.java
@@ -1,4 +1,5 @@
-
+// Escreva um programa que leia 10 numeros inteiros e os armazene em um vetor. Imprima ´
+//o vetor, o maior elemento e a posic¸ao que ele se encontra.
 import java.util.Scanner;
 
 public class Vetor7 {
